7d39e0266a044686b618c092cadc61b3d7e39daa
khriemer2596 <khriemer@gmail.com>
2024-08-12 18:28:39 -0500

    Customized app name and logo. Added new splash screen and app icon.

--- app/src/main/java/com/example/recipegen/MainActivity.kt
+++ app/src/main/java/com/example/recipegen/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.compose.RecipeGenTheme
 
-class MainActivity : ComponentActivity() {
+open class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             RecipeGenTheme {
                 // A surface container using the 'background' color from the theme
